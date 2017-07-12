c1cc981f311be98c34ceedaf60853a1284e80d40
Michael Kapelko <kornerr@gmail.com>
2017-07-12 22:08:49 +0700

    Make Python print current time

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 
-#include <iostream>
+#include <Python.h>
 
-int main() {
+#include <iostream>
+#include <string>
+
+const char *pycode = R"(
+import time
+
+print('Today is', time.ctime(time.time()))
+)";
+
+int main(int argc, char *argv[])
+{
     printf("Embed Python\n");
 
+    wchar_t *program = Py_DecodeLocale(argv[0], 0);
+    if (!program)
+    {
+        printf("Cannot decode program name from first CLI argument. Aborting.\n");
+        exit(1);
+    }
+    Py_SetProgramName(program);
+    Py_Initialize();
+
+    PyRun_SimpleString(pycode);
+
+    Py_Finalize();
+    PyMem_RawFree(program);
 
     return 0;
 }
